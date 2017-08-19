55b73747c04f66ed705d7bb5af0e4b61df0b2550
yl1994 <446042055@qq.com>
2017-08-19 20:22:37 +0800

    comment ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,12 @@
 class CommentsController < ApplicationController
   
   def create
-    c = Comment.new(comment_params)
-    c.save
-    redirect_to c.issue
+    @comment = Comment.new(comment_params)
+    @comment.save
+    # redirect_to c.issue
+    respond_to do |f|
+      f.js	
+    end 	
   end
 
   private
